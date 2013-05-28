703cab45a142da435daf2e949b0894b0790c4974
aalmiray <aalmiray@gmail.com>
2013-05-28 16:19:51 +0200

    Added lesson1 with gradle/maven setup

---
+++ lesson1/src/main/java/lesson1/HelloWorld.java
@@ -0,0 +1,37 @@
+package lesson1;
+ 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Original source can be found at
+ * http://docs.oracle.com/javafx/2/get_started/hello_world.htm
+ */
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
